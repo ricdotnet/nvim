027e45391a67c8b660f79b8bb1b44cc2a624c049
Ricardo Rocha <me@rrocha.uk>
2025-06-17 11:52:55 +0100

    chore: add autoformat on save on eslint

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -40,6 +40,14 @@ vim.lsp.config('tailwindcss', {
 })
 
 vim.lsp.config('eslint', {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      buffer = bufnr,
+      callback = function()
+        vim.cmd 'LspEslintFixAll'
+      end,
+    })
+  end,
   filetypes = {
     'javascript',
     'javascriptreact',
