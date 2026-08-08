fca89ecc42c92a4b64d3945730ccd41c6599dd95
= <me@rrocha.uk>
2026-08-08 11:30:23 +0100

    chore: add more configs

--- lua/configs/plugins/conform.lua
+++ lua/configs/plugins/conform.lua
@@ -4,18 +4,21 @@ return {
   opts = {
     formatters = {
       prettierd = {
-        require_cwd = true,
+        cwd = function(self, ctx)
+          -- Searches upwards from the current buffer to find the /ui project root
+          return vim.fs.root(ctx.buf, { '.prettierrc.json', '.prettierrc', 'package.json' })
+        end,
       },
     },
 
     formatters_by_ft = {
       lua = { 'stylua' },
-      css = { 'prettierd', 'prettier' },
-      html = { 'prettierd', 'prettier' },
-      vue = { 'prettierd', 'prettier' },
-      javascript = { 'prettierd', 'prettier' },
-      typescript = { 'prettierd', 'prettier' },
-      json = { 'prettierd', 'prettier' },
+      css = { 'prettierd' },
+      html = { 'prettierd' },
+      vue = { 'prettierd' },
+      javascript = { 'prettierd' },
+      typescript = { 'prettierd' },
+      json = { 'prettierd' },
       -- javascriptjsx = { "prettierd", "prettier" },
       -- typescripttsx = { "prettierd", "prettier" },
     },
--- lua/configs/plugins/lsp.lua
+++ lua/configs/plugins/lsp.lua
@@ -18,7 +18,16 @@ local function lsp_config()
   lsp.config('ts_ls', {
     capabilities = capabilities,
     cmd = { 'typescript-language-server', '--stdio' },
-    filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
+    init_options = {
+      plugins = {
+        {
+          name = '@vue/typescript-plugin',
+          location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+          languages = { 'vue' },
+        },
+      },
+    },
   })
 
   lsp.config('vue_ls', {
